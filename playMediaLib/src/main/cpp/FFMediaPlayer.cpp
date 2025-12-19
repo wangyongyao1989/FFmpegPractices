974f27b7718225c9e1272c20d99e2e5c774fabb1
wangyongyao1989 <wangyongyao1989@163.com>
2025-12-19 15:03:24 +0800

    音视频同步——代码格式化

--- playMediaLib/src/main/cpp/FFMediaPlayer.cpp
+++ playMediaLib/src/main/cpp/FFMediaPlayer.cpp
@@ -47,6 +47,8 @@ FFMediaPlayer::~FFMediaPlayer() {
     pthread_cond_destroy(&mStateCond);
     pthread_mutex_destroy(&mPacketMutex);
     pthread_cond_destroy(&mPacketCond);
+    pthread_mutex_destroy(&mBufferMutex);
+    pthread_cond_destroy(&mBufferReadyCond);
 }
 
 bool FFMediaPlayer::init(const char *url, jobject surface) {
@@ -60,7 +62,9 @@ bool FFMediaPlayer::init(const char *url, jobject surface) {
 
 bool FFMediaPlayer::prepare() {
     LOGI("prepare()");
-
+    playAudioInfo =
+            "prepare() \n";
+    PostStatusMessage(playAudioInfo.c_str());
     if (mState != STATE_INITIALIZED) {
         LOGE("prepare called in invalid state: %d", mState);
         return false;
@@ -233,25 +237,10 @@ bool FFMediaPlayer::stop() {
     pthread_cond_broadcast(&mPacketCond);
     pthread_cond_broadcast(&mAudioInfo.audioCond);
     pthread_cond_broadcast(&mVideoInfo.videoCond);
-
-    if (helper.player) {
-        helper.stop();
-    }
+    pthread_cond_broadcast(&mBufferReadyCond);
 
     cleanupANativeWindow();
 
-    // 清空缓冲区队列
-    if (helper.bufferQueueItf) {
-        (*helper.bufferQueueItf)->Clear(helper.bufferQueueItf);
-    }
-
-    pthread_mutex_destroy(&mBufferMutex);
-    pthread_cond_destroy(&mBufferReadyCond);
-
-    // 清理缓冲区
-    for (int i = 0; i < NUM_BUFFERS; i++) {
-        delete[] mBuffers[i];
-    }
 
     // 等待线程结束
     if (mAudioInfo.codecContext) {
@@ -268,6 +257,19 @@ bool FFMediaPlayer::stop() {
     clearAudioFrames();
     clearVideoFrames();
 
+    if (helper.player) {
+        helper.stop();
+    }
+
+    // 清空缓冲区队列
+    if (helper.bufferQueueItf) {
+        (*helper.bufferQueueItf)->Clear(helper.bufferQueueItf);
+    }
+
+    // 清理缓冲区
+    for (int i = 0; i < NUM_BUFFERS; i++) {
+        delete[] mBuffers[i];
+    }
     mState = STATE_STOPPED;
     return true;
 }
@@ -459,13 +461,8 @@ void FFMediaPlayer::audioPlay() {
         pthread_mutex_lock(&mBufferMutex);
 
         while (mQueuedBufferCount >= NUM_BUFFERS) {
-            LOGI("Waiting for buffer slot, queued: %d", mQueuedBufferCount.load());
-            playAudioInfo =
-                    "Waiting for buffer slot, queued:" + to_string(mQueuedBufferCount) + " \n";
-            PostStatusMessage(playAudioInfo.c_str());
             pthread_cond_wait(&mBufferReadyCond, &mBufferMutex);
         }
-
 
         // 从音频队列获取一帧数据
         AudioFrame *aframe = getAudioFrame();
@@ -519,14 +516,34 @@ void FFMediaPlayer::audioPlay() {
                     // 成功入队，更新状态
                     mQueuedBufferCount++;
                     mCurrentBuffer = (mCurrentBuffer + 1) % NUM_BUFFERS;
-                    LOGI("111Buffer enqueued successfully: %d bytes, buffer index: %d, queued: %d",
-                         bytesDecoded, mCurrentBuffer, mQueuedBufferCount.load());
                 }
             } else if (outSamples < 0) {
                 LOGE("swr_convert failed: %d", outSamples);
             }
-
             delete aframe;
+        } else {
+            // 队列为空，送入静音数据
+            static uint8_t silence[4096] = {0};
+            // 将缓冲区加入播放队列
+            SLresult result = (*helper.bufferQueueItf)->Enqueue(helper.bufferQueueItf,
+                                                                silence, sizeof(silence));
+
+            if (result != SL_RESULT_SUCCESS) {
+                LOGE("Failed to enqueue buffer: %d, error: %s",
+                     result, getSLErrorString(result));
+
+                if (result == SL_RESULT_BUFFER_INSUFFICIENT) {
+                    // 等待一段时间后重试
+                    pthread_mutex_unlock(&mBufferMutex);
+                    usleep(10000); // 10ms
+                    pthread_mutex_lock(&mBufferMutex);
+                }
+                break;
+            } else {
+                // 成功入队，更新状态
+                mQueuedBufferCount++;
+                mCurrentBuffer = (mCurrentBuffer + 1) % NUM_BUFFERS;
+            }
 
         }
         pthread_mutex_unlock(&mBufferMutex);
@@ -663,9 +680,8 @@ VideoFrame *FFMediaPlayer::decodeVideoFrame(AVFrame *frame) {
 }
 
 void FFMediaPlayer::renderVideoFrame(VideoFrame *vframe) {
-    LOGW("renderVideoFrame===============");
     if (!mNativeWindow || !vframe) {
-        return ;
+        return;
     }
 
     ANativeWindow_Buffer windowBuffer;
@@ -681,7 +697,7 @@ void FFMediaPlayer::renderVideoFrame(VideoFrame *vframe) {
     }
 
     // 优化拷贝：检查 stride 是否匹配
-    uint8_t* dst = static_cast<uint8_t*>(windowBuffer.bits);
+    uint8_t *dst = static_cast<uint8_t *>(windowBuffer.bits);
     int dstStride = windowBuffer.stride * 4;  // 目标步长（字节）
     int srcStride = mRgbFrame->linesize[0];   // 源步长（字节）
 
@@ -776,7 +792,7 @@ void FFMediaPlayer::clearVideoPackets() {
 void FFMediaPlayer::putAudioFrame(AudioFrame *frame) {
     pthread_mutex_lock(&mAudioInfo.audioMutex);
     while (mAudioInfo.audioQueue.size() >= mAudioInfo.maxAudioFrames && !mExit) {
-        LOGI("putAudioFrame Waiting for buffer slot, audioQueue: %d", mAudioInfo.audioQueue.size());
+//        LOGI("putAudioFrame Waiting for buffer slot, audioQueue: %d", mAudioInfo.audioQueue.size());
         pthread_cond_wait(&mAudioInfo.audioCond, &mAudioInfo.audioMutex);
     }
 
@@ -790,7 +806,7 @@ void FFMediaPlayer::putAudioFrame(AudioFrame *frame) {
 void FFMediaPlayer::putVideoFrame(VideoFrame *frame) {
     pthread_mutex_lock(&mVideoInfo.videoMutex);
     while (mVideoInfo.videoQueue.size() >= mVideoInfo.maxVideoFrames && !mExit) {
-        LOGI("putVideoFrame Waiting for buffer slot, videoQueue: %d", mVideoInfo.videoQueue.size());
+//        LOGI("putVideoFrame Waiting for buffer slot, videoQueue: %d", mVideoInfo.videoQueue.size());
         pthread_cond_wait(&mVideoInfo.videoCond, &mVideoInfo.videoMutex);
     }
 
@@ -888,18 +904,25 @@ void FFMediaPlayer::syncVideo(double pts) {
     double diff = videoTime - audioTime;
 
     // 同步阈值
-    const double syncThreshold = 0.01;
-    const double maxFrameDelay = 0.1;
+    const double syncThreshold = 0.01;  // 10ms同步阈值
+    const double maxFrameDelay = 0.1;   // 最大100ms延迟
+    LOGE("视频减音频的差值：%ld", diff);
 
     if (fabs(diff) < maxFrameDelay) {
         if (diff <= -syncThreshold) {
             // 视频落后，立即显示
-//            LOGW("视频落后，立即显示===============");
+            LOGW("视频落后，立即显示===============");
+            playAudioInfo =
+                    "视频落后，立即显示=============== \n";
+            PostStatusMessage(playAudioInfo.c_str());
             return;
         } else if (diff >= syncThreshold) {
             // 视频超前，延迟显示
-            int delay = (int) (diff * 1000000); // 转换为微秒
-//            LOGW("视频超前，延迟显示===============%d", delay);
+            int delay = (int) (diff * 1000000 ); // 转换为微秒
+            LOGW("视频超前，延迟显示===============%d", delay);
+            playAudioInfo =
+                    "视频超前，延迟显示===============" + to_string(delay) + " \n";
+            PostStatusMessage(playAudioInfo.c_str());
             usleep(delay);
         }
     }
@@ -952,7 +975,9 @@ bool FFMediaPlayer::openCodecContext(int *streamIndex, AVCodecContext **codecCon
 
 bool FFMediaPlayer::initOpenSLES() {
     LOGI("initOpenSLES()");
-
+    playAudioInfo =
+            "initOpenSLES() \n";
+    PostStatusMessage(playAudioInfo.c_str());
     // 创建 OpenSL 引擎与引擎接口
     SLresult result = helper.createEngine();
     if (!helper.isSuccess(result)) {
@@ -1021,9 +1046,7 @@ void FFMediaPlayer::processBufferQueue() {
     // 缓冲区已播放完成，减少计数
     if (mQueuedBufferCount > 0) {
         mQueuedBufferCount--;
-        LOGI("Buffer processed, queued count: %d", mQueuedBufferCount.load());
     }
-
     // 通知解码线程有可用的缓冲区槽位
     pthread_cond_signal(&mBufferReadyCond);
     pthread_mutex_unlock(&mBufferMutex);
@@ -1060,7 +1083,8 @@ bool FFMediaPlayer::initANativeWindow() {
         return false;
     }
 
-    mSwsContext = sws_getContext(mVideoInfo.width, mVideoInfo.height, mVideoInfo.codecContext->pix_fmt,
+    mSwsContext = sws_getContext(mVideoInfo.width, mVideoInfo.height,
+                                 mVideoInfo.codecContext->pix_fmt,
                                  mVideoInfo.width, mVideoInfo.height, AV_PIX_FMT_RGBA,
                                  SWS_BICUBIC, nullptr, nullptr, nullptr);
     if (!mSwsContext) {
@@ -1089,6 +1113,9 @@ bool FFMediaPlayer::initANativeWindow() {
     }
 
     LOGI("ANativeWindow initialization successful");
+    playAudioInfo =
+            "ANativeWindow initialization successful \n";
+    PostStatusMessage(playAudioInfo.c_str());
     return true;
 }
 
--- playMediaLib/src/main/java/com/wangyao/playaudiolib/PlayMediaOperate.java
+++ playMediaLib/src/main/java/com/wangyao/playaudiolib/PlayMediaOperate.java
@@ -129,7 +129,7 @@ public class PlayMediaOperate {
 
 
     private void CppStatusCallback(String status) {
-        Log.d(TAG, "CppStatusCallback: " + status);
+//        Log.d(TAG, "CppStatusCallback: " + status);
         if (mOnStatusMsgListener != null) {
             mOnStatusMsgListener.onStatusMsg(status);
         }
